ba490aeda6fe73113a122a5fde9988962dbb7e2b
riya1689 <riyaratri24@gmail.com>
2025-09-29 17:18:41 +0600

    (advanced) lift up state in array by comparing reference

--- react-rest-countries/src/components/Countries/Countries.jsx
+++ react-rest-countries/src/components/Countries/Countries.jsx
@@ -8,11 +8,19 @@ const Countries = ({countriesPromise}) => {
 
     const handleVisitedCountries =(country) =>{
         console.log('country visited clicked to be added',country);
+        const newVisitedCountries =[...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
     }
-    console.log(countries);
+    console.log(visitedCountries);
     return (
         <div>
             <h1>Traveling Countries: {countries.length}</h1>
+            <h3>Traveled so far: {visitedCountries.length} </h3>
+            <ol>
+                {
+                    visitedCountries.map(country => <li>{country.name.common}</li>)
+                }
+            </ol>
             <div className='countries'>
                 {
                 countries.map(country => <Country 
